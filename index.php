145bb5447696c96a975716da5e84a37dd0186973
tgadboy <viktormenschikow@gmail.com>
2025-09-09 22:02:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-// Простейший рабочий вариант
 header('Content-Type: application/json');
-
-$response = [
-    'url' => 'https://github.com',
-    'confirmation_code' => 'test_123'
-];
-
-echo json_encode($response);
+echo '{"url":"https://github.com","confirmation_code":"working"}';
 ?>
